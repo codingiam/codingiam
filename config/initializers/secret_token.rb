0fc2b5714bf5ce5ff76501d52f512bf89da3a3ca
Doru Catalin Budai <doru.budai@gmail.com>
2012-04-06 11:41:46 +0300

    Bootstrapped CodingIAm for JRuby.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CodingIAm::Application.config.secret_token = '2e1fc98f94a8af8d63c6d9ce90b8c85d116ec80d7aec395778fee78dde3f954d072ee3fbf72cbbb3c838d769c044ebbc0f66f29968a543d480c49485787ec347'

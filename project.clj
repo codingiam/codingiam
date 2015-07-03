57bfc82a78810a02c533d9459a125e041d6b060e
Doru Budai <doru.budai@gmail.com>
2015-07-03 18:37:57 +0300

    Update libraries

--- project.clj
+++ project.clj
@@ -1,13 +1,12 @@
-(defproject codingiam "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-3126"]]
-  :plugins [[lein-cljsbuild "1.0.5"]]
-  :cljsbuild {
-    :builds [{:source-paths ["app/assets/clojurescripts"]
-              :notify-command ["notify-send"]
-              :compiler {:output-to "app/assets/javascripts/codingiam.js"
-                         :output-dir "tmp/assets/javascripts"
-                         :optimizations :simple
-                         :output-wrapper true
-                         :print-input-delimiter true
-                         :pretty-print true}}]})
+(defproject codingiam "0.0.1"
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "0.0-3308"]]
+  :plugins [[lein-cljsbuild "1.0.6"]]
+  :cljsbuild {:builds  {:development {:source-paths ["app/assets/clojurescripts"]
+                                      :notify-command ["notify-send"]
+                                      :compiler {:output-to "app/assets/javascripts/codingiam.js"
+                                                 :output-dir "tmp/assets/javascripts"
+                                                 :optimizations :simple
+                                                 :output-wrapper true
+                                                 :print-input-delimiter true
+                                                 :pretty-print true}}}})
